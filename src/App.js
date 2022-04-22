4674d7f6f20c697f25e0c4da168a175b2c7dacba
Jenrielle Gaon <jenrielle@ubx.ph>
2022-04-23 00:48:23 +0800

    added credit/debit amounts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { TextField } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import moment from 'moment';
-import React, { useState } from 'react';
-import './App.scss';
-import { getDifferenceInDays, roundOff } from './helpers';
+/* eslint-disable no-mixed-operators */
+import { TextField } from '@mui/material'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import moment from 'moment'
+import React, { useState } from 'react'
+import './App.scss'
+import { getDifferenceInDays, roundOff } from './helpers'
 
 function App() {
   const [discountingDate, setdiscountingDate] = useState(moment().startOf('day'))
@@ -20,16 +21,16 @@ function App() {
     atc: 0,
     processingFee: 0,
     bankCharge: 0,
-  });
+  })
   const [chargeTo, setchargeTo] = useState("BUYER")
 
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
+    setValues({ ...values, [prop]: event.target.value })
+  }
 
   const handleSelectChargeTo = (event) => {
-    setchargeTo(event?.target?.value);
-  };
+    setchargeTo(event?.target?.value)
+  }
 
   const vatRate = values?.vatRate / 100
   const factorRate = values?.factorRate / 100
@@ -38,7 +39,7 @@ function App() {
   const bankCharge = values?.bankCharge / 100
 
   const daysBeforeDueDate = getDifferenceInDays(dueDate, discountingDate)
-  const divisor = daysBeforeDueDate < 365 ? 360 : 365 
+  const divisor = daysBeforeDueDate < 365 ? 360 : 365
 
   const taxWithheld = roundOff(values?.outstandingAmount / (vatRate + 1) * atc)
   const acceptedAmount = roundOff(values?.outstandingAmount - taxWithheld)
@@ -47,6 +48,13 @@ function App() {
   const receivableAmount = chargeTo === "BUYER" ? roundOff(acceptedAmount + totalCharges) : acceptedAmount
   const netProceeds = chargeTo === "BUYER" ? roundOff(acceptedAmount - factorAmount) : roundOff(acceptedAmount - factorAmount - totalCharges)
   const accrualAmount = chargeTo === "BUYER" ? roundOff(factorAmount + totalCharges) : factorAmount
+  const creditSellerCasa = netProceeds
+  const creditEarned = chargeTo === "BUYER" ? 0 : totalCharges
+  const creditUnearned = accrualAmount
+  // const creditTotal = roundOff(creditSellerCasa + creditEarned + creditUnearned)
+  const creditDebitB2B = chargeTo === "BUYER" ? receivableAmount : acceptedAmount
+  const creditEarnedAccrual = roundOff(factorAmount / daysBeforeDueDate) || 0
+  const creditUnearnedAccrual = roundOff(factorAmount / daysBeforeDueDate) || 0
 
   return (
     <div className="app-main">
@@ -58,7 +66,7 @@ function App() {
               label="Discounting Date"
               value={discountingDate}
               onChange={(newValue) => {
-                setdiscountingDate(newValue);
+                setdiscountingDate(newValue)
               }}
               renderInput={(params) => <TextField {...params} />}
             />
@@ -66,7 +74,7 @@ function App() {
               label="Due Date"
               value={dueDate}
               onChange={(newValue) => {
-                setdueDate(newValue);
+                setdueDate(newValue)
               }}
               renderInput={(params) => <TextField {...params} />}
             />
@@ -84,7 +92,7 @@ function App() {
           />
           <TextField
             id="vatRate"
-            label="Vat Rate (%)"
+            label={`Vat Rate (${values?.vatRate / 100})`}
             variant="outlined"
             placeholder="0%"
             fullWidth
@@ -93,7 +101,7 @@ function App() {
           />
           <TextField
             id="factorRate"
-            label="Factor Rate (%)"
+            label={`Factor Rate (${values?.factorRate / 100})`}
             variant="outlined"
             placeholder="0%"
             fullWidth
@@ -102,7 +110,7 @@ function App() {
           />
           <TextField
             id="atc"
-            label="ATC (%)"
+            label={`ATC (${values?.atc / 100})`}
             variant="outlined"
             placeholder="0%"
             fullWidth
@@ -111,7 +119,7 @@ function App() {
           />
           <TextField
             id="processingFee"
-            label="Processing Fee (%)"
+            label={`Processing Fee (${values?.processingFee / 100})`}
             variant="outlined"
             placeholder="0%"
             fullWidth
@@ -120,7 +128,7 @@ function App() {
           />
           <TextField
             id="bankCharge"
-            label="Bank Charges (%)"
+            label={`Bank Charges (${values?.bankCharge / 100})`}
             variant="outlined"
             placeholder="0%"
             fullWidth
@@ -142,46 +150,90 @@ function App() {
           </FormControl>
         </div>
         <div className="results-container">
-          <div className="results">
-            <span>Days Before Due Date</span>
-            <span>{daysBeforeDueDate}</span>
+          <div className="left-container">
+            <div className="results">
+              <span>Days Before Due Date</span>
+              <span>{daysBeforeDueDate}</span>
+            </div>
+            <div className="results">
+              <span>Divisor</span>
+              <span>{divisor}</span>
+            </div>
+            <div className="results">
+              <span>Tax Withheld</span>
+              <span>{taxWithheld}</span>
+            </div>
+            <div className="results">
+              <span>Accepted Amount</span>
+              <span>{acceptedAmount}</span>
+            </div>
+            <div className="results">
+              <span>Factor Amount</span>
+              <span>{factorAmount}</span>
+            </div>
+            <div className="results">
+              <span>Total Charges</span>
+              <span>{totalCharges}</span>
+            </div>
+            <div className="results">
+              <span>Receivable Amount</span>
+              <span>{receivableAmount}</span>
+            </div>
+            <div className="results">
+              <span>Net Proceeds</span>
+              <span>{netProceeds}</span>
+            </div>
+            <div className="results">
+              <span>Accrual Amount</span>
+              <span>{accrualAmount}</span>
+            </div>
           </div>
-          <div className="results">
-            <span>Divisor</span>
-            <span>{divisor}</span>
-          </div>
-          <div className="results">
-            <span>Tax Withheld</span>
-            <span>{taxWithheld}</span>
-          </div>
-          <div className="results">
-            <span>Accepted Amount</span>
-            <span>{acceptedAmount}</span>
-          </div>
-          <div className="results">
-            <span>Factor Amount</span>
-            <span>{factorAmount}</span>
-          </div>
-          <div className="results">
-            <span>Total Charges</span>
-            <span>{totalCharges}</span>
-          </div>
-          <div className="results">
-            <span>Receivable Amount</span>
-            <span>{receivableAmount}</span>
-          </div>
-          <div className="results">
-            <span>Net Proceeds</span>
-            <span>{netProceeds}</span>
-          </div>
-          <div className="results">
-            <span>Accrual Amount</span>
-            <span>{accrualAmount}</span>
+          <div className="right-container">
+            <div className="results">
+              <span>Debit AR/GL</span>
+              <span>{receivableAmount}</span>
+            </div>
+            <div className="results">
+              <span>Credit Seller CASA</span>
+              <span>{creditSellerCasa}</span>
+            </div>
+            <div className="results">
+              <span>Credit Earned (Bank to Seller)</span>
+              <span>{creditEarned}</span>
+            </div>
+            <div className="results">
+              <span>Credit Unearned (Bank to Seller)</span>
+              <span>{creditUnearned}</span>
+            </div>
+            {/* <div className="results">
+              <span>Credit Total</span>
+              <span>{creditTotal}</span>
+            </div> */}
+            <div className="results">
+              <span>Credit AR</span>
+              <span>{creditDebitB2B}</span>
+            </div>
+            <div className="results">
+              <span>Debit Buyer CASA</span>
+              <span>{creditDebitB2B}</span>
+            </div>
+            <div className="results">
+              <span>Total Accrual Amount</span>
+              <span>{factorAmount}</span>
+            </div>
+            <div className="results">
+              <span>Credit Earned (Daily Accrual)</span>
+              <span>{creditEarnedAccrual}</span>
+            </div>
+            <div className="results">
+              <span>Credit Unearned (Daily Accrual)</span>
+              <span>{creditUnearnedAccrual}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
